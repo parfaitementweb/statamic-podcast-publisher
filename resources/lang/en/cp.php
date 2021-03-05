0166ce0d6bf28298f851e4705e1a287134eb7002
Alexis <alexis@parfaitementweb.com>
2021-03-05 16:02:22 +0100

    Dashboard and localization

--- resources/lang/en/cp.php
+++ resources/lang/en/cp.php
@@ -1,5 +1,21 @@
 <?php return [
-    'dashboard' => "Dashboard",
+    'dashboard' => "Podcast Publisher",
     'settings' => "Podcast Settings",
     'episodes' => "Episodes",
+    'create_first_episode' => "Create your first episode",
+    'view_episodes' => "View your episodes",
+    'add_episode' => "Add an episode",
+    'edit_settings' => "Edit Settings",
+    'feed' => "Podcast feed",
+    'view_feed' => "Visit your feed page",
+    'introduction' => "Thank you for choosing Podcast Publisher to manage your podcast feed and website. <br/>We have made things easy for you start. Here is a quick summary of all things you need to know.
+<ul class='mt-1 text-xs'>
+    <li class='ml-3'>1. <strong>Edit your podcast information.</strong><br/>Choose your name, description, language and a beautiful image.<br>Podcast settings is a customizable Statamic Global set.</li>
+    <li class='ml-3 mt-1'>2. <strong>Add an episode.</strong><br/>Choose the audio file, add show notes and select the publishing date.<br>Episodes are stored in a customizable Statamic Collection within it's dedicated blueprint.</li>
+    <li class='ml-3 mt-1'>3. <strong>Submit your feed.</strong><br/>Submit your feed URL to any podcast distributors (Apple, Gooogle, Spotify). You have complete control and are not limited here.<br>We've automatically created a Feed page and linked to the correct layout.</li>
+</ul>",
+    'distributors' => "Podcast Distributors",
+    'visit' => "Visit",
+    'guide' => "submission guide",
+    'footer' => 'Get more Statamic addons at <span class="text-grey-70">Parfaitement Web</span>',
 ];
--- resources/views/cp/dashboard.blade.php
+++ resources/views/cp/dashboard.blade.php
@@ -5,14 +5,55 @@
 
     <h1 class="mt-1 mb-2">{{ __('statamic-podcast-publisher::cp.dashboard') }}</h1>
 
-    @if($first_episode)
-        <a href="{{ cp_route('collections.entries.create', ['collection' => 'episodes', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">Create your first episode</a>
-    @endif
+    <div class="bg-white p-2 rounded shadow-sm">
+        <p>{!! __('statamic-podcast-publisher::cp.introduction') !!}</p>
+    </div>
 
-    @if(! $first_episode)
-        <a href="{{ cp_route('collections.show', ['collection' => 'episodes', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">View your episodes</a>
-        <a href="{{ cp_route('collections.entries.create', ['collection' => 'episodes', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">Add an episode</a>
-    @endif
+    <div class="flex flex-col lg:flex-row justify-between my-3 lg:space-x-2">
+        <div>
+            <h2 class="mb-1">{{ __('statamic-podcast-publisher::cp.episodes') }}</h2>
 
-    <a href="{{ cp_route('globals.update', ['global_set' => 'podcast', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn btn-primary">Edit Settings</a>
+            @if($first_episode)
+                <a href="{{ cp_route('collections.entries.create', ['collection' => 'episodes', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">{{ __('statamic-podcast-publisher::cp.create_first_episode') }}</a>
+            @endif
+
+            @if(! $first_episode)
+                <a href="{{ cp_route('collections.show', ['collection' => 'episodes', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">{{ __('statamic-podcast-publisher::cp.view_episodes') }}</a>
+                <a href="{{ cp_route('collections.entries.create', ['collection' => 'episodes', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">{{ __('statamic-podcast-publisher::cp.add_episode') }}</a>
+            @endif
+
+            <h2 class="mt-3 mb-1">{{ __('statamic-podcast-publisher::cp.settings') }}</h2>
+            <a href="{{ cp_route('globals.update', ['global_set' => 'podcast', 'site' => \Statamic\Facades\Site::current()]) }}" class="btn">{{ __('statamic-podcast-publisher::cp.edit_settings') }}</a>
+
+            <h2 class="mt-3 mb-1">{{ __('statamic-podcast-publisher::cp.feed') }}</h2>
+            <a target="_blank" href="{{ url('feed') }}" class="btn btn-primary">{{ __('statamic-podcast-publisher::cp.view_feed') }}</a>
+        </div>
+
+        <div class="flex flex-col space-y-1 mt-3 lg:mt-0">
+
+            <h2 class="mb-1">{{ __('statamic-podcast-publisher::cp.distributors') }}</h2>
+
+            <div class="flex justify-start items-center space-x-2">
+                <a href="https://podcastsconnect.apple.com/my-podcasts" target="_blank" class="btn">{{ __('statamic-podcast-publisher::cp.visit') }} <strong>Apple Connect</strong></a>
+                <a href="https://itunespartner.apple.com/podcasts/" target="_blank" class="text-xs text-blue-500">Apple {{ __('statamic-podcast-publisher::cp.guide') }}</a>
+            </div>
+            <div class="flex justify-start items-center space-x-2">
+                <a href="https://podcastsmanager.google.com" target="_blank" class="btn">{{ __('statamic-podcast-publisher::cp.visit') }} <strong>Google Podcasts</strong></a>
+                <a href="https://support.google.com/podcast-publishers/answer/9482981" target="_blank" class="text-xs text-blue-500">Google {{ __('statamic-podcast-publisher::cp.guide') }}</a>
+            </div>
+            <div class="flex justify-start items-center space-x-2">
+                <a href="https://podcasters.spotify.com/" target="_blank" class="btn">{{ __('statamic-podcast-publisher::cp.visit') }} <strong>Spotify for Podcasters</strong></a>
+                <a href="https://support.spotifyforpodcasters.com/hc/en-us/articles/360043487932-Getting-your-podcast-on-Spotify" target="_blank" class="text-xs text-blue-500">Spotify {{ __('statamic-podcast-publisher::cp.guide') }}</a>
+            </div>
+            <div class="flex justify-start items-center space-x-2">
+                <a href="https://overcast.fm/podcasterinfo/" target="_blank" class="btn">{{ __('statamic-podcast-publisher::cp.visit') }} <strong>Overcast</strong></a>
+                <a href="https://overcast.fm/podcasterinfo" target="_blank" class="text-xs text-blue-500">Overcast {{ __('statamic-podcast-publisher::cp.guide') }}</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center mt-6">
+        <a href="https://www.parfaitementweb.com/?utm_source=statamic-cp-podcast-publisher" target="_blank" class="bg-white rounded-full px-3 py-1 shadow-sm text-xs text-grey-60">{!! __('statamic-podcast-publisher::cp.footer') !!}</a>
+    </div>
+
 @stop
